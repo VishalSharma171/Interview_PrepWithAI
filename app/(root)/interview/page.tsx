9e603fee44a3c87a09adfe61f9f8e6771fce50c3
Vishal Sharma <sharma.s@ahduni.edu.in>
2025-05-11 16:21:45 +0530

    show generated interview on homepage

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.actions";
 
+const Page = async () => {
+  const user = await getCurrentUser();
 
-const Page = () => {
-    return (
-        <>
-            <h3>Interview Generation</h3>
+  return (
+    <>
+      <h3>Interview generation</h3>
 
-            <Agent userName="You" userId="user1" type="generate" />
-        </>
-    )
-}
-export default Page
+      <Agent
+        userName={user?.name!}
+        userId={user?.id}
+        profileImage={user?.profileURL}
+        type="generate"
+      />
+    </>
+  );
+};
+
+export default Page;
